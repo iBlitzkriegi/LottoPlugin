5503d5572c4345e54558e66d47cf51d15b4e3a7c
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-11-24 01:00:17 -0500

    Add Util method to convert UUID without repeated code

--- src/main/java/net/runebrire/lottoplugin/util/Util.java
+++ src/main/java/net/runebrire/lottoplugin/util/Util.java
@@ -2,9 +2,15 @@ package net.runebrire.lottoplugin.util;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Util {
     public static void sendMessage(CommandSender commandSender, String message) {
         commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
+
+    public static String getUniqueId(Player player) {
+        return player.getUniqueId().toString();
+    }
+
 }
