0ce1edc96bf93dcc4a8adc283d770e23afe85a17
Delphi Systems <mlitvinkox96@gmail.com>
2018-11-23 20:55:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# LotteryPlugin
+Not really sure how this is going to work yet, possibly as a 50/50 draw? Players buy tickets with a configurable value that have a number on them and at a configurable time (or after a configurable amount of time has passed) one of the numbers given out is randomly chosen as the winner. Winning ticket can be exchanged for money or other prizes? 50/50 draw assumes the prize is half the value of all the tickets purchased, but the prize should ultimately be configurable as well.
+
+If the tickets are real items rather than just something you keep track of with commands, and if the items each have unique lore, then they won't stack and players won't be able to spam too many tickets. Could be a pro or a con, might want to implement multiple options for how people can implement this plugin on their servers.
+
+Slot machines using maps could probably be a thing too, but that's a bit more complex...
---
+++ src/main/java/net/runebrire/lottoplugin/LottoPlugin.java
@@ -0,0 +1,47 @@
+package net.runebrire.lottoplugin;
+
+import net.runebrire.lottoplugin.commands.lottery;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class LottoPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Plugin is now Loading");
+        if(!loadConfig()){
+            getLogger().severe("Unable to load config files. Disabling plugin.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+        this.getCommand("lottery").setExecutor(new lottery());
+        getServer().getConsoleSender().sendMessage("LotteryPlugin Enabled");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage("LotteryPlugin Disabled.");
+    }
+
+    private boolean loadConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("Created config.yml");
+                saveDefaultConfig();
+            }
+            return true;
+        } catch (Exception unknownError) {
+            getLogger().info("Unable to load config.yml");
+            unknownError.printStackTrace();
+            return false;
+        }
+    }
+}
