5f4f972dede7ce2f14aa4ed83b0dac34b995a09c
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-11-25 17:11:39 -0500

    Update LotteryDataLoader.java

--- src/main/java/net/runebrire/lottoplugin/util/LotteryDataLoader.java
+++ src/main/java/net/runebrire/lottoplugin/util/LotteryDataLoader.java
@@ -1,29 +1,32 @@
 package net.runebrire.lottoplugin.util;
 
 import net.runebrire.lottoplugin.LottoPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class LotteryDataLoader {
 
     private static LottoPlugin plugin = TicketHandler.getPlugin();
 
     public static void saveTickets() {
+//        if (TicketHandler.getTicketMap().isEmpty()) return;
         for (String uuid : TicketHandler.getTicketMap().keySet()) {
-            plugin.getLotteryPlayerConfig().set("tickets." + uuid, TicketHandler.getTicketMap().get(uuid));
-
+            plugin.getCurrentLottery().set("tickets." + uuid, TicketHandler.getTicketMap().get(uuid));
         }
         try {
-            plugin.lotteryPlayer.save(plugin.lotteryPlayerFile);
+            plugin.currentLottery.save(plugin.currentLotteryFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void loadTickets() {
-        if (!plugin.getLotteryPlayerConfig().contains("tickets")) return;
-        for (String uuid : plugin.getLotteryPlayerConfig().getConfigurationSection("tickets").getKeys(false)) {
-            TicketHandler.setTicket(uuid, plugin.getLotteryPlayerConfig().getString("tickets." + uuid));
+        if (!plugin.getCurrentLottery().contains("tickets")) return;
+        FileConfiguration currentLottery = plugin.getCurrentLottery();
+        for (String uuid : currentLottery.getConfigurationSection("tickets").getKeys(false)) {
+            TicketHandler.setTicket(uuid, currentLottery.getString("tickets." + uuid));
         }
     }
 
